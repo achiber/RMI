458c13e7d909644f79831d148e1d6bae8c96981d
albert <achiber@163.com>
2017-10-30 20:33:04 +0800

    修改HelloClient

--- HelloClient.java
+++ HelloClient.java
@@ -11,6 +11,7 @@ public class HelloClient {
 			IHello rhello = (IHello) Naming.lookup("rmi://127.0.0.1:8888/RHello");
 			System.out.println(rhello.sayHello("world"));
 			System.out.println(rhello.sum(12, 120));
+			System.out.println(rhello.getUser());
 		} catch (MalformedURLException | RemoteException | NotBoundException e) {
 			e.printStackTrace();
 		}
